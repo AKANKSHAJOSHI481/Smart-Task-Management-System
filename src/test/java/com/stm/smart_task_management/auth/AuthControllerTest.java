2fac566cce468f664b773f70a9408d948e5be152
Akanksha Joshi <joshiakanksha481@gmail.com>
2026-05-10 15:30:00 +0530

    added Intergration tests and CI

---
+++ src/test/java/com/stm/smart_task_management/auth/AuthControllerTest.java
@@ -0,0 +1,45 @@
+package com.stm.smart_task_management.auth;
+
+import com.stm.smart_task_management.auth.dto.RegisterRequest;
+import com.stm.smart_task_management.integrationTest.AbstractIntegrationTest;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+public class AuthControllerTest extends AbstractIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    void shouldRegisterUser() throws Exception{
+        RegisterRequest request = new RegisterRequest(
+                "Akanksha Joshi",
+                "joshiakanksha@email.com",
+                "password"
+        );
+        mockMvc.perform(
+                post("/api/auth/register")
+                        .header(
+                                "X-Tenant-ID",
+                                "tenant_1"
+                        )
+                        .contentType(
+                                MediaType.APPLICATION_JSON
+                        )
+                        .content(
+                                objectMapper.writeValueAsString(request)
+                        )
+        )
+                .andExpect(status().isOk());
+    }
+}
---
+++ src/test/java/com/stm/smart_task_management/integrationTest/AbstractIntegrationTest.java
@@ -0,0 +1,26 @@
+package com.stm.smart_task_management.integrationTest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class AbstractIntegrationTest {
+    @Container
+    static PostgreSQLContainer<?> postgre =
+            new PostgreSQLContainer<>("postgres:latest")
+                    .withDatabaseName("test_db")
+                    .withUsername("admin")
+                    .withPassword("admin");
+
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry){
+        registry.add("spring.datasource.url", postgre::getJdbcUrl);
+        registry.add("spring.datasource.username", postgre::getUsername);
+        registry.add("spring.datasource.password", postgre::getPassword);
+    }
+}
