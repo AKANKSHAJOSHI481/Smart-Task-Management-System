bc637007ef28ac2ec7e1a2f69dd8f695faf84400
Akanksha Joshi <joshiakanksha481@gmail.com>
2026-05-10 19:04:59 +0530

    made some changes

--- src/test/java/com/stm/smart_task_management/integrationTest/AbstractIntegrationTest.java
+++ src/test/java/com/stm/smart_task_management/integrationTest/AbstractIntegrationTest.java
@@ -5,24 +5,31 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 @SpringBootTest
 @Testcontainers
 public abstract class AbstractIntegrationTest {
     @Container
     static PostgreSQLContainer<?> postgre =
-            new PostgreSQLContainer<>("postgres:latest")
+            new PostgreSQLContainer<>("postgres:16")
                     .withDatabaseName("test_db")
                     .withUsername("admin")
                     .withPassword("admin");
-
+    @Container
+    static GenericContainer<?> redis =
+            new GenericContainer<>(DockerImageName.parse("redis:7"))
+                    .withExposedPorts(6379);
     @DynamicPropertySource
     static void configureProperties(DynamicPropertyRegistry registry){
         registry.add("spring.datasource.url", postgre::getJdbcUrl);
         registry.add("spring.datasource.username", postgre::getUsername);
         registry.add("spring.datasource.password", postgre::getPassword);
+        registry.add("spring.data.redis.host", redis::getHost);
+        registry.add("spring.data.redis.port", () -> redis.getMappedPort(6379));
     }
 }
